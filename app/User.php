1d90781bcdcf95b8cb1c5dd0a6a6f96008b6b30c
Andy Melentev <melentev.av@gmail.com>
2020-06-20 17:23:59 +0500

    rework users table

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    const ROLES = [
+        1 => 'Администратор',
+        2 => 'Участник',
+    ];
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,12 +11,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = new \App\User([
-            'name' => 'Василий Пупкин',
-            'email' => 'admin@test.com',
-            'password' => bcrypt('secret'),
-        ]);
 
-        $user->save();
+        $users = [
+            [
+                'name' => 'User Admin',
+                'email' => 'admin@test.com',
+                'password' => bcrypt('secret'),
+                'role' => 1
+            ],
+            [
+                'name' => 'User Customer',
+                'email' => 'user@test.com',
+                'password' => bcrypt('secret'),
+                'role' => 2
+            ],
+        ];
+
+
+        foreach ($users as $user) {
+            $user = new \App\User([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => $user['password'],
+                'role' => $user['role'],
+            ]);
+
+            $user->save();
+        }
+
+
+
     }
 }
